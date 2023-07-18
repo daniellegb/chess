e3af81167e5ef29799ffd51c02752c6293310df0
Danielle Balogh <daniellegb@protonmail.com>
2023-07-18 13:28:08 -0300

    Printing the board

--- src/layer/ChessPiece.java
+++ src/layer/ChessPiece.java
@@ -1,14 +1,25 @@
 package layer;
 
+import board.Board;
 import board.Piece;
 import board.Position;
 
 public class ChessPiece extends Piece {
-	
+
+
 	private Color color;
 	private int moveCount;
 	
 	//All methods bellow needs to be implemented!
+	
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
 	
 	public ChessPosition getChessPosition() {
 		return null;
